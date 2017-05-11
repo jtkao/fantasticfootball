c326c1d3adff2157cc0456b3a53325115613db82
Joshua Kao <joshuakao1904@gmail.com>
2017-05-10 21:03:46 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
-var Sequelize = require("sequelize");
+var mysql = require("mysql");
 
-var sequelize = new Sequelize("heroku_37d45f748722cd4", "becf12872a09ab", "ac5b754b", {
+var connection = mysql.createConnection({
+  port: 3306,   
   host: "us-cdbr-iron-east-03.cleardb.net",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
+  user: "becf12872a09ab",
+  password: "ac5b754b",
+  database: "heroku_37d45f748722cd4"
 });
 
-module.exports = sequelize;
+// connect
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
