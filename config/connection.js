8ea201ba0d5920b8afac2168a4c7914843b27659
Joshua Kao <joshuakao1904@gmail.com>
2017-05-10 20:59:38 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("fantasy_football", "becf12872a09ab", "ac5b754b", {
+var sequelize = new Sequelize("heroku_37d45f748722cd4", "becf12872a09ab", "ac5b754b", {
   host: "us-cdbr-iron-east-03.cleardb.net",
   dialect: "mysql",
   pool: {
