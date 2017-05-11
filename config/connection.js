a764984b9f8a6682c60a0c0e0a8e063f4e8e683e
Joshua Kao <joshuakao1904@gmail.com>
2017-05-10 20:48:30 -0700

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("fantasy_football", "root", "root", {
-  host: "localhost",
+var sequelize = new Sequelize("fantasy_football", "becf12872a09ab", "ac5b754b", {
+  host: "us-cdbr-iron-east-03.cleardb.net",
   dialect: "mysql",
   pool: {
     max: 5,
