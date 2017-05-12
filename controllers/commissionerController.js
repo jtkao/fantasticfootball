8a8aa3dfe8ad8d201b9daa7cf7ffbed8f4a09278
Joshua Kao <joshuakao1904@gmail.com>
2017-05-12 10:33:16 -0700

    more testing

--- controllers/commissionerController.js
+++ controllers/commissionerController.js
@@ -23,7 +23,8 @@ module.exports = function(app) {
 
                 db.sequelize.query('SELECT `position` FROM `t_players` WHERE `id` = ' + '"' + req.body.id + '"')
                     .then(function(id) {
-                        console.log(id[0]);
+                        console.log("COMU RAW PLAYER ID, " id[0])
+                        var playerId = id[0][0]
                         var fantasyPoints = scoring.playerScore(scoreRubric, "QB", req.body);
                         console.log(fantasyPoints)
 
